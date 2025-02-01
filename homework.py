96dfadd6060de7d0d3b1a11cec07d7ce26109f53
aaravhajare <hajareaarav66@gmail.com>
2025-02-01 15:51:53 +0530

    homework.py

---
+++ homework.py
@@ -0,0 +1,14 @@
+
+#making variable and printing
+
+aahan = "27/7/2012"
+drijesh = "2/8/2012"
+Himansh = "5/6/2011"
+a = "6/9/2012"
+b = "16/8/2013"
+
+print ("DOB of Aahan is" , aahan)
+print ("DOB of Drijesh is" , drijesh)
+print ("DOB of Himansh is" , Himansh)
+print ("DOB of A is" , a)
+print ("DOB of B is" , b)
