7dedf0ccaac70082534aa4331a0dde3a4a5c2ea1
aaravhajare <hajareaarav66@gmail.com>
2025-02-01 15:30:10 +0530

    classwork

---
+++ classwork.py
@@ -0,0 +1,15 @@
+
+#printing hello
+print ("Welcome this program")
+
+#prnting number
+print (6)
+
+#printing next line
+print ("Hi I am Aarev \n hello")
+
+#printing combination
+print ("Hi I am Aarav" , 6)
+
+#printing end
+print ("hello How are you" , end="$")
