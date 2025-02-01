474a3bf17283ded1b62e1a2ad8064f9843b27f6c
aaravhajare <hajareaarav66@gmail.com>
2025-02-01 15:37:40 +0530

    classwork2

---
+++ classwork2.py
@@ -0,0 +1,22 @@
+
+#creating variable
+
+x=5
+y = 6
+
+# printing variable
+print (x)
+print(y)
+print(x+y)
+
+#string variable
+
+a = "hello"
+
+print(a,x)
+
+# taking input
+ 
+name = input("Enter your name")
+print ("\n Hello" , name , "Welcome to home")
+
